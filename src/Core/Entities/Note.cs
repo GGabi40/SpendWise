5a148cd00981dba9bf0841fe49e7eec4be33f526
MurielSalbador <salbadormuriel@gmail.com>
2025-11-05 14:29:24 -0300

    Add Folder Entitis

---
+++ src/Core/Entities/Note.cs
@@ -0,0 +1,15 @@
+namespace SpendWise.Core.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public bool IsPinned { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Relación con Usuario
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ src/Core/Entities/User.cs
@@ -0,0 +1,14 @@
+namespace SpendWise.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+        // Relaciones
+        public ICollection<Transaction>? Transactions { get; set; }
+        public ICollection<Note>? Notes { get; set; }
+    }
+}
