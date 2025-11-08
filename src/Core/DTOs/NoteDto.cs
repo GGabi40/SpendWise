2fbf3dcb43adacb0a1047e2832474b423ac5c4b7
ggabi40 <gabibc2000@hotmail.com>
2025-11-08 11:42:40 -0300

    Add pin and unpin functionality to notes; refactor CreateNoteRequest and NoteService

--- src/Core/DTOs/NoteDto.cs
+++ src/Core/DTOs/NoteDto.cs
@@ -2,8 +2,9 @@ namespace SpendWise.Core.DTOs
 {
     public class NoteDto
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public bool IsPinned { get; set; } = false;
+        public bool IsPinned { get; set; }
     }
 }
